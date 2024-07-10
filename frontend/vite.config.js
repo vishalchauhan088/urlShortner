eadefe0ad8da56ee95c29c7b9a32aa096a646033
Vishal Chauhan <vishalchauhan0688@gmail.com>
2024-07-10 09:48:47 +0530

    production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+//
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
    
     proxy:{
       
-      '/api': 'http://localhost:5000' 
+      '/api': 'https://url-shortner-backend-zeta.vercel.app' 
       
     },
   },
